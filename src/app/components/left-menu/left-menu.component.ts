67d6f7c26fec460399d43b3f37c209f8ca82df76
Dominik Chrastecky <dominik@chrastecky.cz>
2025-04-11 16:59:40 +0200

    Add basic structure

--- src/app/components/left-menu/left-menu.component.ts
+++ src/app/components/left-menu/left-menu.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {TranslocoPipe} from "@jsverse/transloco";
 
 @Component({
   selector: 'app-left-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    TranslocoPipe
+  ],
   templateUrl: './left-menu.component.html',
   styleUrl: './left-menu.component.scss'
 })
--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {TranslocoPipe} from "@jsverse/transloco";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslocoPipe,
+    RouterLink
+  ],
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.scss'
 })
